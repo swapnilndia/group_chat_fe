8895b50bd5ee446e7c9859b728516bc5e8d17c72
Swapnil Katiyar <swapnilndia@Swapnils-MacBook-Air.local>
2024-07-12 21:08:50 +0530

    new commit

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -2,8 +2,6 @@
 import io from "socket.io-client";
 
 // const socket = io("http://localhost:3000");
-const socket = io(
-  "http://ec2-3-109-78-168.ap-south-1.compute.amazonaws.com:3000"
-);
+const socket = io("https://group-chat-be.swapnilndia.xyz");
 
 export default socket;
